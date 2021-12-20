108dd6ade34fb58c4b3547379118a420690b8802
Romessss <krakozibr@mail.ru>
2021-12-21 02:55:44 +0300

    19.12.2021

--- lesson4.c
+++ lesson4.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
-void lesson_4()
+#include <string.h>
+
+int main()
 {
-    char bin[] = "b";
+    return 0;
+}
+
+void binRec(int n, char *rslt)
+{
+    if (n <= 0)
+        return;
+
+    binRec(n / 2, rslt);
+    strcat(rslt, (n % 2 == 0) ? "0" : "1");
+}
+
+int recPower(int n, int ex)
+{
+    if (ex <= 0)
+        return 1;
+    return n * recPower(n, ex - 1);
+}
+
+int recPowerFast(int n, int ex)
+{
+    if (ex <= 0)
+        return 1;
+    if (ex % 2 == 0)
+        return recPowerFast(n * n, ex / 2);
+    else
+        return n * recPowerFast(n, ex - 1);
+}
+
+void lesson4()
+{
+    char bin[] = "b`";
     printf("%i : ", 5);
     binRec(5, bin);
-    printf("%s\n,bin");
+    printf("%s\n", bin);
+
+    printf("5^7 : %d \n", recPower(5, 7));
+    printf("5^7 : %d\n", recPowerFast(5, 7));
 }
